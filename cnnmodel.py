7eed94b6ccbb0d432f5e064a346a6755abf6d33e
fyfhaolihai666 <fu194@purdue.edu>
2018-11-11 21:34:26 -0500

    add code to log scalars to tensorboard Visualize loss, accuracy, and the graph with tensorboard

--- cnnmodel.py
+++ cnnmodel.py
@@ -19,22 +19,25 @@ def cnn_model_fn(features, labels, mode):
     input_layer = tf.reshape(features["x"], [-1, 40, 30, 3])
     print(input_layer)
     input_layer = tf.cast(input_layer, dtype=tf.float32)
-    input_norm = tf.layers.batch_normalization(input_layer)
-    print(input_norm)
+    # input_norm = tf.layers.batch_normalization(input_layer)
+    # print(input_norm)
     # Convolutional Layer #1
-    conv1 = tf.layers.conv2d(
-        inputs=input_norm,
-        filters=16,
-        kernel_size=[5, 5],
-        padding='valid',
-        activation=tf.nn.relu)
-    print(conv1)
+    with tf.name_scope('conv1'):
+        conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=16,
+            kernel_size=[5, 5],
+            padding='valid',
+            activation=tf.nn.relu,
+            name='conv1')
+
+        print(conv1)
 
     # Pooling Layer #1
     # conv1_norm = tf.layers.batch_normalization(conv1)
     pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], strides=2)
-    pool1 = tf.layers.batch_normalization(pool1)
-    print(pool1)
+    # pool1 = tf.layers.batch_normalization(pool1)
+    # print(pool1)
 
     # # Convolutional Layer #2 and Pooling Layer #2
     conv2 = tf.layers.conv2d(
@@ -46,16 +49,16 @@ def cnn_model_fn(features, labels, mode):
     print(conv2)
     # conv2 = tf.layers.batch_normalization(conv2)
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-    pool2 = tf.layers.batch_normalization(pool2)
-    print(pool2)
+    # pool2 = tf.layers.batch_normalization(pool2)
+    # print(pool2)
 
     pool2_flat = tf.reshape(pool2, [-1, 4*6*32])
     # dconv1=tf.layers.conv2d_transpose()
     # Dense Layer
     dense1 = tf.layers.dense(inputs=pool2_flat, units=256, activation=tf.nn.relu)  # 36/2
-    dropout1 = tf.layers.dropout(
-        inputs=dense1, rate=0, training=mode == tf.estimator.ModeKeys.TRAIN)
-    dense2 = tf.layers.dense(inputs=dropout1, units=80, activation=tf.nn.relu)
+    # dropout1 = tf.layers.dropout(
+    #     inputs=dense1, rate=0, training=mode == tf.estimator.ModeKeys.TRAIN)
+    dense2 = tf.layers.dense(inputs=dense1, units=80, activation=tf.nn.relu)
     dense3 = tf.layers.dense(inputs=dense2, units=2, activation=tf.nn.relu)
     # dropout = tf.layers.dropout(
     #     inputs=dense3, rate=0.2, training=mode == tf.estimator.ModeKeys.TRAIN)
@@ -79,11 +82,10 @@ def cnn_model_fn(features, labels, mode):
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
     # Calculate Loss (for both TRAIN and EVAL modes)
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=output)
-    # loss = tf.losses.mean_squared_error(
-    #     labels=labels, predictions=output)
 
-    # Configure the Training Op (for TRAIN mode)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=output)
+    tf.summary.scalar('loss', loss)
+
     if mode == tf.estimator.ModeKeys.TRAIN:
         rate = tf.train.exponential_decay(learning_rate=0.1, global_step=tf.train.get_global_step(), decay_steps=100,
                                           decay_rate=0.9, staircase=False)
@@ -93,12 +95,12 @@ def cnn_model_fn(features, labels, mode):
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     # Add evaluation metrics (for EVAL mode)
-    if mode == tf.estimator.ModeKeys.EVAL:
-        # celoss= tf.losses.softmax_cross_entropy(onehot_labels=labels,logits=output)
-
+    with tf.name_scope('accuracy'):
         accuracy = tf.metrics.accuracy(labels=lableclass,
                                        predictions=predicted,
                                        name='acc_op')
+        tf.summary.scalar('accuracy', accuracy)
+    if mode == tf.estimator.ModeKeys.EVAL:
         eval_metric_ops = {"accuricy":accuracy}
         return tf.estimator.EstimatorSpec(
             mode=mode, loss=loss,eval_metric_ops=eval_metric_ops)
@@ -116,7 +118,8 @@ def main(unused_argv):
     eval_labels = datay[300:340]
 
     estimator = tf.estimator.Estimator(
-        model_fn=cnn_model_fn
+        model_fn=cnn_model_fn,
+        model_dir='tensorboard/model'
     )
 
     # Set up logging for predictions
@@ -126,7 +129,7 @@ def main(unused_argv):
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": train_data},
         y=train_labels,
-        batch_size=16,
+        batch_size=128,
         num_epochs=None,
         shuffle=True,
         num_threads=4)
@@ -139,7 +142,7 @@ def main(unused_argv):
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": eval_data},
         y=eval_labels,
-        num_epochs=1,
+        num_epochs=64,
         shuffle=False)
     eval_results = estimator.evaluate(input_fn=eval_input_fn)
     print(eval_results)
